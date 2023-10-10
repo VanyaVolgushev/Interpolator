d5e7afce1dc30b1e3a3378d0e6f40734aa29b332
Ivan Volgushev <ivo.volgushev@yandex.ru>
2023-10-10 03:02:13 +0300

    added Newtonian interpolation, fixed Lagrange inetrpolation call args, typos.

--- interpolator.py
+++ interpolator.py
@@ -46,17 +46,40 @@ def LagrangeInterpolation(table, x, n):
     return res
 
 
+#get divided differences
+def GetDiv(table, arguments):
+    if len(arguments) == 2:
+        return (table[arguments[1]] - table[arguments[0]])/(arguments[1] - arguments[0])
+    else :
+        numOfArguments = len(arguments)
+        return (GetDiv(table, arguments[1 : numOfArguments]) - GetDiv(table, arguments[0 : numOfArguments - 1]))/(
+            arguments[numOfArguments - 1] - arguments[0])
+
 # table: dict {x0 : f(x0)}
 def NewtonInterpolation(table, x, n):
-    pass
+    arguments = list(table.keys())
+    result = 0
+    rightMultiplicant = 1
 
+    pairIndex = 0
+    result += table[arguments[0]]
+    for arguementValuePair in table:
+        if pairIndex == 0:
+            pairIndex += 1
+            continue
+
+        rightMultiplicant *= (x - arguments[pairIndex - 1])
+        leftMultiplicant = GetDiv(table, arguments[0 : pairIndex + 1])
+        result += leftMultiplicant * rightMultiplicant
+        pairIndex += 1
+    return result
 
 if __name__ == "__main__":
     print("Задача алгебраического интерполирования\n"
           "Интерполяционный многочлен в форме Ньютона и в форме Лагранжа\n")
     print("Вариант №6")
     print("f(x): x^2 / (1 + x^2)\n"
-          "a = -0.5, b = 1\n"
+          "A = -0.5, B = 1\n"
           "m+1 = 31, n = 8")
     func = lambda y: y ** 2 / (1 + y ** 2)
 
@@ -83,9 +106,14 @@ if __name__ == "__main__":
         print_table(sorted_table)
         print()
 
-        lagrange = LagrangeInterpolation(table, x, n)
+        lagrange = LagrangeInterpolation(sorted_table, x, n)
         print(f"L({x}) = {lagrange}")
         print(f"|f(x)-L(x)|: {abs(func(x) - lagrange)}")
+        print()
+
+        newton = NewtonInterpolation(sorted_table, x, n)
+        print(f"N({x}) = {newton}")
+        print(f"|f(x)-N(x)|: {abs(func(x) - newton)}")
         print()
 
         exit_program = input('Завершить программу? (y/n): ').lower().strip() == 'y'
